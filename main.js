496473518206003e823f530c000778bca28d86fa
CommandMaster64 <81648386+CommandMaster64@users.noreply.github.com>
2022-09-29 16:33:54 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 function st() {
     if (window.location.href.includes("sc=(")) {
+	for (let i = 0; i < Games.length; i+=2) {
+	    if (Games[i].toLocaleLowerCase().includes(window.location.href.split("sc=(")[1].split(")")[0].toLocaleLowerCase())) {
+		document.getElementById("feat").innerHTML = '<div onmousedown="window.open(https://commandmaster64.github.io/UB_Games.io/play=(' + Games[i] + ')" id="tileClick" style="border: 2px solid black"><h1>' + Games[i] + '</h1></div>';
+	    }
+	}
 	document.getElementById("hdr-1").innerHTML = "UB Games";
 	document.getElementById("feat").innerHTML = "";
 	document.getElementById("searchBar").value = window.location.href.split("sc=(")[1].split(")")[0];
+    }
+    if (window.location.href.includes("play=(")) {
+	let game = window.location.href.split("play=(")[1].split(")")[0];
+	document.getElementById("hdr-1").innerHTML = "UB Games";
+	document.getElementById("feat").innerHTML = Games[Games.indexOf(game)+1];
     }
 }
 document.getElementById("searchButton").addEventListener('click', () => {
